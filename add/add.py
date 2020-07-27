eb7da7468e2862ecd2f96025d5900f2adf8417ef
Göran Paues <goran.paues@tradedoubler.com>
2020-07-27 21:20:50 +0200

    Solution for "add" morsel

--- add/add.py
+++ add/add.py
@@ -1,1 +1,8 @@
-print("Hello world")
+def add(matrix1, matrix2):
+    matrixsum = []     
+    for i in range(len(matrix1)):
+        sublist = []
+        for j in range(len(matrix1[i])):
+            sublist.append(matrix1[i][j] + matrix2[i][j])
+        matrixsum.append(sublist)
+    return matrixsum
