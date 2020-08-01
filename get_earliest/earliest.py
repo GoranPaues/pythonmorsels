bd4af7bfbb34d98e82f554224126aca353d217be
Göran Paues <goran.paues@tradedoubler.com>
2020-08-01 22:25:12 +0200

    Learnings for get_earliest

--- get_earliest/earliest.py
+++ get_earliest/earliest.py
@@ -1,10 +1,6 @@
 def get_earliest(date1, date2):
-    date1_month = date1[:2]
-    date1_day = date1[3:5]
-    date1_year = date1[6:]
-    date2_month = date2[:2]
-    date2_day = date2[3:5]
-    date2_year = date2[6:]
+    date1_month, date1_day, date1_year = date1.split("/")
+    date2_month, date2_day, date2_year = date2.split("/")
     if date1_year > date2_year:
         return date2
     elif date1_year < date2_year:
---
+++ get_earliest/get_earliest.md
@@ -0,0 +1,8 @@
+
+# Learnings from exercise "get_earliest"
+
+* In functions like sort() and min(), a key can be specified. This key is then used for sorting.
+
+* Split is very effective together with multiple assignments.
+
+* Whole tuples can be compared with one another
